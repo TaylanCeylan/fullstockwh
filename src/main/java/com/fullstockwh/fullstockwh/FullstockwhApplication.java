f5d72e84bb45b78b4c0bcfe5af32d421f0274d8b
Taylan Ceylan <69519588+TaylanCeylan@users.noreply.github.com>
2026-03-31 19:38:55 +0300

    Initialize

---
+++ src/main/java/com/fullstockwh/fullstockwh/FullstockwhApplication.java
@@ -0,0 +1,13 @@
+package com.fullstockwh.fullstockwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstockwhApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(FullstockwhApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fullstockwh/fullstockwh/FullstockwhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstockwh.fullstockwh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstockwhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
