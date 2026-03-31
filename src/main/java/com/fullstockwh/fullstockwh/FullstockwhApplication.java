e5f0178444b82eec5c9c4a18d08b7014c9897fd7
Taylan Ceylan <69519588+TaylanCeylan@users.noreply.github.com>
2026-03-31 19:42:55 +0300

    Update FullstockwhApplication.java

--- src/main/java/com/fullstockwh/fullstockwh/FullstockwhApplication.java
+++ src/main/java/com/fullstockwh/fullstockwh/FullstockwhApplication.java
@@ -1,5 +1,6 @@
 package com.fullstockwh.fullstockwh;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class FullstockwhApplication
 {
 	public static void main(String[] args)
 	{
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(FullstockwhApplication.class, args);
 	}
 }
