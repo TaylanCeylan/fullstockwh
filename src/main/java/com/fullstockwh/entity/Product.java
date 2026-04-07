de1c41578a1c1ac1fae0db5238805a3a26f3136a
Sertaç Çakır <sertac.cakir.eng@gmail.com>
2026-04-07 23:31:44 +0300

    Product classes added
    Added/Updated:
    Product Entity class
    Product Service class
    Product Controller class
    Product Repository
    Product Service Interface
    Product Repository Interface

--- src/main/java/com/fullstockwh/entity/Product.java
+++ src/main/java/com/fullstockwh/entity/Product.java
@@ -1,18 +1,30 @@
 package com.fullstockwh.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Table(name = "product")
-@Data
-@AllArgsConstructor
+@Table(name = "products")
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Product
 {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer stockQuantity;
+    
 }
