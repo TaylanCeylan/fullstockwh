07d17a85e326adacc42cd6a6b098141ddcf60ed7
Sk4rpe <gokhankiyici10@hotmail.com>
2026-05-23 16:37:42 +0300

    Updated maps and charts in shipping module

--- src/main/java/com/fullstockwh/shipment/ShipmentServiceImpl.java
+++ src/main/java/com/fullstockwh/shipment/ShipmentServiceImpl.java
@@ -163,6 +163,8 @@ class ShipmentServiceImpl implements ShipmentService
                 .orderStatus(order.getStatus())
                 .shippingFee(shipment.getShippingFee())
                 .distanceKm(shipment.getDistanceKm())
+                .destinationLat(shipment.getDestinationLat())
+                .destinationLon(shipment.getDestinationLon())
                 .destinationCity(address != null ? address.getCity() : "-")
                 .destinationDistrict(address != null ? address.getDistrict() : "-")
                 .orderedAt(order.getOrderDate())
